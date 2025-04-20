54f39b45536c86eebca65e423a8067ef77f90d37
TIMISONG-dev <dima.pilaer@gmail.com>
2025-04-20 13:42:16 +0300

    drivers/cpufreq: Rework thermal pressure

--- drivers/cpufreq/qcom-cpufreq-hw.c
+++ drivers/cpufreq/qcom-cpufreq-hw.c
@@ -156,8 +156,12 @@ static unsigned long limits_mitigation_notify(struct cpufreq_qcom *c,
 				GENMASK(7, 0);
 		freq = DIV_ROUND_CLOSEST_ULL(freq * c->xo_rate, 1000);
 		if (policy) {
-			capacity = freq * max_capacity;
-			capacity /= policy->cpuinfo.max_freq;
+			/* Convert limited freq to reduced capacity */
+			capacity = mult_frac(max_capacity, freq, policy->cpuinfo.max_freq);
+	
+			/* Don't pass boost capacity to scheduler */
+			if (capacity > max_capacity)
+				capacity = max_capacity;
 		}
 	} else {
 		if (!policy)
